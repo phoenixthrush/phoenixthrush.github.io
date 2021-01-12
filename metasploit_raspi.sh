1f25fdb0da469aa8c190a4a1c309541364bdb219
Phoenixthrush <44802077+Phoenixthrush@users.noreply.github.com>
2021-01-12 02:14:02 +0100

    Create metasploit_raspi.sh

---
+++ metasploit_raspi.sh
@@ -0,0 +1,14 @@
+#!\bin\bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install build-essential zlib1g zlib1g-dev libxml2 libxml2-dev libxslt-dev locate libreadline6-dev libcurl4-openssl-dev git-core libssl-dev libyaml-dev openssl autoconf libtool ncurses-dev bison curl wget postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev -y 
+sudo apt install ruby-dev -y
+cd /opt
+sudo git clone https://github.com/rapid7/metasploit-framework.git
+cd metasploit-framework
+sudo apt install git-core postgresql curl nmap gem libsqlite3-dev -y
+sudo gem install wirble sqlite3 bundler
+sudo gem install nokogiri
+sudo gem install bundle
+bundle install
+./msfconsole
