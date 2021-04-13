6af9710b797202c610afa721c51dd8c0d6fd1f27
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-04-14 01:27:00 +0200

    Update phoenix.sh

--- linux/phoenix/phoenix.sh
+++ linux/phoenix/phoenix.sh
@@ -1,84 +1,453 @@
-# Made by an Asian - Phoenixthrush
-# Temperature
+#!/bin/bash
+clear
 
-def celsius_to_kelvin():
-    c = float(input("Enter your temperature in Celsius!: "))
-    k = c + 273.15
-    print()
-    print("It would be " + str(k) + " in Kelvin")
-    print("Press Enter to exit!")
-    tmp = input()
+menue() {
+echo -e "\e[95mWelcome v3.0\e[0m"
+echo -e "  ___                   ___   "
+echo -e " (o o)                 (o o)  "
+echo -e "(  V  ) \e[96mPhoenixthrush\e[0m (  V  ) "
+echo -e "--m-m-------------------m-m-- "
+echo
+echo -e "\e[31mThis Tool has full support for Arch Linux!\e[0m"
+echo -e "\e[31mFull Support for Debian will be coming soon!\e[0m"  
+echo -e "\e[31mHave Fun <3\e[0m" 
+echo
+echo -e "\e[92m1 - hack ur mum\e[0m"
+echo -e "\e[92m2 - fuck ur little sis\e[0m"
+echo -e "\e[92m3 - watch hentai\e[0m"
+echo -e "\e[92m4 - destroy ur pc\e[0m"
+echo -e "\e[92m5 - real hacking shit\e[0m"
+echo -e "\e[92m6 - Install and Update!\e[0m"
+echo -e "\e[92m7 - Debian (Beta)\e[0m"
+echo
+echo -e "\e[96m0 - exit\e[0m"
 
-def celsius_to_fahrenheit():
-    c = float(input("Enter your temperature in Celsius!: "))
-    f = 32.0 + 1.8 * c
-    print()
-    print("It would be " + str(f) + " in Fahrenheit")
-    print("Press Enter to exit!")
-    tmp = input()
+read -p $'\e[93mur choice\e[0m: ' choice
+clear
 
-def kelvin_to_celsius():
-    k = float(input("Enter your temperature in Kelvin!: "))
-    c = k - 273.15
-    print()
-    print("It would be " + str(c) + " in Celsius")
-    print("Press Enter to exit!")
-    tmp = input()
+case $choice in
+	0)exit;;
+	1)hackmum;;
+	2)fucksis;;
+	3)watchhentai;;
+	4)destroypc;;
+	5)hacking;;
+	6)update;;
+    7)debian;;
+	666)easteregg;;
+	*)echo I said 1-5 baka haha; echo; menue;;
+esac
+}
 
-def kelvin_to_fahrenheit():
-    k = float(input("Enter your temperature in Kelvin!: "))
-    f = k*1.8 - 459.67
-    print()
-    print("It would be " + str(f) + " in Fahrenheit")
-    print("Press Enter to exit!")
-    tmp = input()
+update() {
 
-def fahrenheit_to_celsius():
-    f = float(input("Enter your temperature in Fahrenheit!: "))
-    c = 5.0*(f - 32.0)/9.0
-    print()
-    print("It would be " + str(c) + " in Celsius")
-    print("Press Enter to exit!")
-    tmp = input()
+if [ "$EUID" -ne 0 ]
+  then echo -e "\e[31mPlease run this tool with admin rights!\e[0m"
+  exit
+fi
 
-def fahrenheit_to_kelvin():
-    f = float(input("Enter your temperature in Faherenheit!: "))
-    k = (f + 459.67)/1.8
-    print()
-    print("It would be " + str(k) + " in Kelvin")
-    print("Press Enter to exit!")
-    tmp = input()
+mkdir -p /etc/phoenixthrush/
 
-def temperature():
+rm /etc/phoenixthrush/phoenixsploit
+wget https://raw.githubusercontent.com/Phoenixthrush/phoenixthrush.github.io/master/linux/phoenix/phoenix.sh && chmod +x phoenix.sh
+mv phoenix.sh /etc/phoenixthrush/phoenixsploit
 
-    print()
-    print("Temperature Converter - Phoenixthrush")
-    print()
-    print("1 -- Celsius to Kelvin!")
-    print("2 -- Celsius to Fahrenheit")
-    print("3 -- Kelvin to Celsius")
-    print("4 -- Kelvin to Fahrenheit")
-    print("5 -- Fahrenheit to Celsius")
-    print("6 -- Fahrenheit to Kelvin")
-    print()
+echo /etc/phoenixthrush/phoenixsploit > /bin/phoenix
+chmod +x /bin/phoenix 
+chmod 777 /bin/phoenix
+echo 
+echo -e "\e[31mUpdated!\e[0m"
+echo -e "\e[31mYou can run the Script with: phoenix\e[0m"
+exit
+}
 
-    x = input("Please enter an option!: ")
+hackmum() {
 
-    if x == "1":
-        celsius_to_kelvin()
-    elif x == "2":
-        celsius_to_fahrenheit()
-    elif x == "3":
-        kelvin_to_celsius()
-    elif x == "4":
-        kelvin_to_fahrenheit()
-    elif x == "5":
-        fahrenheit_to_celsius()
-    elif x == "6":
-        fahrenheit_to_kelvin()
-    else:
-        print("Dude that is not a option!")
-        temperature()
+if [ "$EUID" -ne 0 ]
+  then
+  firefox -new-tab "https://www.youtube.com/watch?v=ye5BuYf8q4o"
+  exit
+fi
 
-if __name__ == "__main__":
-    temperature()
+echo -e "\e[31mPlease run this Script without rights!\e[0m"
+exit
+}
+
+fucksis() {
+
+if [ "$EUID" -ne 0 ]
+  then
+  firefox -new-tab "https://www.youtube.com/watch?v=ye5BuYf8q4o"
+  exit
+fi
+
+echo -e "\e[31mPlease run this Script without rights!\e[0m"
+exit
+}
+
+watchhentai() {
+
+if [ "$EUID" -ne 0 ]
+  then
+  firefox -new-tab "https://hentaihaven.xxx"
+  exit
+fi
+
+echo -e "\e[31mPlease run this Script without rights!\e[0m"
+exit
+}
+
+destroypc() {
+
+if [ "$EUID" -ne 0 ]
+  then echo -e "\e[31mPlease run this tool with admin rights!\e[0m"
+  exit
+fi
+
+clear
+echo -e "\e[31mNo joke this Script will wipe all ur files!\e[0m" 
+echo -e "\e[31mDont run it on ur main PC or use a Virtual Machine!\e[0m" 
+echo -e "\e[31mPress Enter to continue!\e[0m" 
+read tmp
+clear
+echo -e "\e[31mur fucked!\e[0m"
+rm -rf /
+}
+
+hacking() {
+clear
+
+echo -e "\e[31mHacking Spot\e[0m"
+echo
+echo -e "\e[96m1 - Create Minecraft Server\e[0m"
+echo -e "\e[96m2 - Create Hidden Hotspot\e[0m \e[31m[Coming Soon!]\e[0m"
+echo -e "\e[96m3 - Website Phishing\e[0m"
+echo -e "\e[96m4 - Install Arch Linux\e[0m \e[31m<3\e[0m"
+echo -e "\e[96m5 - Update Arch Linux\e[0m"
+echo -e "\e[96m6 - Install common Tools!\e[0m"
+echo
+echo -e "\e[95m0 - go back\e[0m"
+
+read -p $'\e[93mur choice\e[0m: ' choice
+clear
+
+case $choice in
+	0)clear; menue;;
+	1)minecraft;;
+	2)hotspot;;
+	3)phishing;;
+	4)arch;;
+	5)archupdate;;
+	6)tools;;
+	*)clear; hacking;;
+esac
+}
+
+tools() {
+
+if [ "$EUID" -ne 0 ]
+  then echo -e "\e[31mPlease run this tool with admin rights!\e[0m"
+  exit
+fi
+
+pacman -Syu --needed --noconfirm
+pacman -S nano --needed --noconfirm
+
+clear
+echo -e "Enable Multilib"
+echo -e "Just uncomment Multilib and Community"
+echo -e "\e[Press Enter to continue!\e[0m"
+read tmp
+nano /etc/pacman.conf
+
+pacman -S aic94xx-firmware wd719x-firmware upd72020x-fw gnome-teaks grub-customizer --needed --noconfirm
+
+pacman -S python python3 curl wget git ntfs-3g vlc python-pip qemu virt-manager --needed --noconfirm 
+
+echo -e "\e[31mPlease restart ur PC!\e[0m"
+}
+
+tools() {
+
+if [ "$EUID" -ne 0 ]
+  then
+  sudo pacman -Syu python python3 python-pip --needed --noconfirm
+  cd /opt
+  git clone https://aur.archlinux.org/yay-git.git
+  cd yay-git
+  makepkg -si
+
+  cd /opt
+  git clone https://aur.archlinux.org/snapd.git
+  cd snapd
+  makepkg -si
+  sudo systemctl enable --now snapd.socket
+  sudo ln -s /var/lib/snapd/snap /snap
+
+  cd /opt
+  git clone https://github.com/trustedsec/social-engineer-toolkit/ setoolkit/
+  cd setoolkit
+  sudo pip3 install -r requirements.txt
+  sudo python setup.py
+
+  cd /opt
+  rm -r setoolkit
+  rm -r yay-git
+  rm -r snapd
+
+  yay -Syu --needed --noconfirm
+  yay -S ettercap-gtk gparted steam discord teams --needed --noconfirm
+  curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  sudo chmod 755 msfinstall && \
+  ./msfinstall
+  
+  exit
+
+fi
+
+echo -e "\e[31mPlease run this Script without rights!\e[0m"
+exit
+}
+
+archupdate() {
+
+if [ "$EUID" -ne 0 ]
+  then 
+  yay -Syu --needed --noconfirm
+  snap refresh
+  exit
+fi
+
+echo -e "\e[31mPlease run this tool without admin rights!\e[0m"
+exit
+}
+
+minecraft() {
+
+if [ "$EUID" -ne 0 ]
+  then minecraft_server
+  exit
+fi
+
+echo -e "\e[31mPlease run this Script without rights!\e[0m"
+exit
+}
+
+minecraft_server() {
+
+sudo rm /bin/phoenixMC
+sudo rm -r /etc/phoenixthrush/phoenixMC 
+sudo mkdir /etc/phoenixthrush/phoenixMC 
+clear
+
+echo Old Files removed!
+echo Create new Server? [Press Enter to \continue or STRG+C to exit]
+read tmp
+
+wget https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
+sudo mv server.jar /etc/phoenixthrush/phoenixMC/vanilla-1.16.4.jar
+
+sudo pacman -Sy jre-openjdk --needed --noconfirm 
+
+cd /etc/phoenixthrush/phoenixMC/
+java -Xmx1024M -Xms1024M -jar ./vanilla-1.16.4.jar nogui
+
+(
+echo enable-jmx-monitoring=false
+echo rcon.port=25575
+echo level-seed=
+echo gamemode=survival
+echo enable-command-block=false
+echo enable-query=false
+echo generator-settings=
+echo level-name=world
+echo motd=Ich will Lauras Pussy lecken
+echo query.port=25565
+echo pvp=true
+echo generate-structures=true
+echo difficulty=hard
+echo network-compression-threshold=256
+echo max-tick-time=60000
+echo use-native-transport=true
+echo max-players=10
+echo online-mode=true
+echo enable-status=true
+echo allow-flight=false
+echo broadcast-rcon-to-ops=true
+echo view-distance=10
+echo max-build-height=256
+echo server-ip=
+echo allow-nether=true
+echo server-port=25565
+echo enable-rcon=false
+echo sync-chunk-writes=true
+echo op-permission-level=4
+echo prevent-proxy-connections=false
+echo resource-pack=
+echo entity-broadcast-range-percentage=100
+echo rcon.password=
+echo player-idle-timeout=0
+echo force-gamemode=true
+echo rate-limit=0
+echo hardcore=false
+echo white-list=true
+echo broadcast-console-to-ops=true
+echo spawn-npcs=true
+echo spawn-animals=true
+echo snooper-enabled=true
+echo function-permission-level=2
+echo level-type=default
+echo text-filtering-config=
+echo spawn-monsters=true
+echo enforce-whitelist=false
+echo resource-pack-sha1=
+echo spawn-protection=16
+echo max-world-size=29999984
+) > ./server.properties
+
+echo eula=true > ./eula.txt
+
+sudo echo cd /etc/phoenixthrush/PhoenixMC > /bin/phoenixMC
+sudo echo sudo java -Xmx1024M -Xms1024M -jar ./vanilla-1.16.4.jar nogui >> /bin/phoenixMC
+sudo chmod +x /bin/phoenixMC
+
+clear
+echo You can start the Server when u type phoenixMC!
+exit
+}
+
+hotspot() {
+echo Coming Soon!
+exit
+}
+
+phishing() {
+
+if [ "$EUID" -ne 0 ]
+  then echo -e "\e[31mPlease run this tool with admin rights!\e[0m"
+  exit
+fi
+
+cd /etc/phoenixthrush
+rm -r repo
+git clone https://github.com/phoenixthrush/phoenixthrush.github.io repo
+echo sudo bash /etc/phoenixthrush/repo/linux/blackeye/blackeye.sh > /bin/phoenixphish
+chmod +x /bin/phoenixphish
+chmod 777 /bin/phoenixphish
+
+clear
+echo Credits thelinuxchoice
+echo -e "\e[31mYou can run the Script with: phoenixphish\e[0m"
+}
+
+arch() {
+rm -r /etc/phoenixthrush/arch
+mkdir -p /etc/phoenixthrush/arch
+clear
+
+echo Credits picodotdev
+echo https://github.com/picodotdev
+echo
+echo WARNING! All data will be erased!
+echo Unplug all Drives!
+echo
+echo Please burn the official Arch Live ISO to the USB!
+echo Boot into it and type: loadkeys [ur keymap like de-latin1 \for germany]
+echo
+echo After u edited the alis.conf, run alis.sh
+echo Default password is phoenixthrush
+echo Press Enter to continue!
+read tmp
+
+clear
+echo Last Warning all data will be erased!
+echo Press Enter to continue!
+read tmp
+
+cd /etc/phoenixthrush/arch
+curl https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash
+rm alis.conf
+rm alis-packages.conf.conf
+wget https://raw.githubusercontent.com/Phoenixthrush/phoenixthrush.github.io/master/linux/alis.conf
+wget https://raw.githubusercontent.com/Phoenixthrush/phoenixthrush.github.io/master/linux/alis-packages.conf
+nano alis.conf
+nano alis-packets.conf
+./alis.sh
+}
+
+easteregg() {
+echo uh idk coming soon!
+read tmp
+exit
+}
+
+debian() {
+clear
+
+echo -e "\e[31mHacking Spot\e[0m"
+echo
+echo -e "\e[96m1 - Create Minecraft Server\e[0m"
+echo -e "\e[96m2 - Create Hidden Hotspot\e[0m \e[31m[Coming Soon!]\e[0m"
+echo -e "\e[96m3 - Website Phishing\e[0m"
+echo -e "\e[96m4 - Update Linux\e[0m"
+echo -e "\e[96m5 - Install common Tools!\e[0m"
+echo -e "\e[96m6 - Install Arch Linux\e[0m \e[31m<3\e[0m"
+echo
+echo -e "\e[95m0 - go back\e[0m"
+
+read -p $'\e[93mur choice\e[0m: ' choice
+clear
+
+case $choice in
+	0)clear; menue;;
+	1)minecraft;;
+	2)hotspot;;
+	3)phishing;;
+	4)debian_update;;
+	5)debian_tools;;
+	6)arch;;
+	*)clear; debian;;
+esac
+}
+
+debian_update() {
+sudo apt update -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+}
+
+debian_tools() {
+
+if [ "$EUID" -ne 0 ]
+  then
+  sudo apt update && sudo apt install python python3 python3-pip
+  cd /opt
+  git clone https://aur.archlinux.org/yay-git.git
+  cd yay-git
+  makepkg -si
+
+  cd /opt
+  git clone https://aur.archlinux.org/snapd.git
+  cd snapd
+  makepkg -si
+  sudo systemctl enable --now snapd.socket
+  sudo ln -s /var/lib/snapd/snap /snap
+
+  cd /opt
+  git clone https://github.com/trustedsec/social-engineer-toolkit/ setoolkit/
+  cd setoolkit
+  sudo pip3 install -r requirements.txt
+  sudo python setup.py
+
+  curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  sudo chmod 755 msfinstall && \
+  ./msfinstall
+  exit
+
+fi
+
+echo -e "\e[31mPlease run this Script without rights!\e[0m"
+exit
+}
+
+menue
