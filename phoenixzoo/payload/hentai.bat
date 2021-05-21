e4ac1e168f12eaa780f9bdac6a6d88220db8f319
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-05-21 19:21:02 +0200

    Update hentai.bat

--- phoenixzoo/payload/hentai.bat
+++ phoenixzoo/payload/hentai.bat
@@ -1,2 +1,2 @@
-echo hentai = msgbox (^"sell your kidneys^",4,^"License Agreement^") > %userprofile%\Desktop\payload.bat
-start %userprofile%\Desktop\payload.bat
+echo hentai = msgbox (^"sell your kidneys^",4,^"License Agreement^") > %userprofile%\Desktop\payload.vbs
+start %userprofile%\Desktop\payload.vbs
