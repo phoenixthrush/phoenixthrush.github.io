dfa9ce5eeb621c6adcb63a12a02d933c0812399b
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-06-08 21:28:34 +0200

    Update guess_game.c

--- c/guess_game.c
+++ c/guess_game.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int hidden = 12;
+    system("cls");
+    int hidden;
+    srand(time(NULL));
+    hidden = rand() % 51;
     int guess = 0;
     int guesscount = 0;
-    int guesslimit = 3;
+    int guesslimit = 20;
     int outofguesses = 0;
 
     while (guess != hidden && outofguesses == 0) {
         if (guesscount < guesslimit) {
-            printf("Enter ur number: ");
+            printf("\033[31mEnter ur number (between 0 and 50)\033[00m: ");
             scanf("%d", &guess);
             guesscount++;
+            if (guess < hidden && guess != hidden) {
+                printf("\033[96mbigger\033[00m\n\n");
+            } else if (guess > hidden && guess != hidden) {
+                printf("\033[96mlower\033[00m\n\n");
+            }
         } else {
             outofguesses = 1;
         }
     }
-
     if (outofguesses == 1) {
-        printf("yeah out of guesses wtf");
+        system("cls");
+        printf("\033[31myeah out of guesses\033[00m\n");
+        printf("\033[96mpress enter to continue\033[00m");
+        system("pause >nul");
     } else {
-        printf("you won uh ye");
+        system("cls");
+        printf("\033[31myou won uh ye\033[00m\n");
+        printf("\033[96mpress enter to continue\033[00m");
+        system("pause >nul");
     }
+
+    return 0;
 }
