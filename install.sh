12c879d1a39fbd127381e14851567cdf2101c33a
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-02 17:25:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-curl -s --compressed "https://phoenixthrush.com/repo/KEY.gpg" | sudo apt-key add -
-sudo curl -s --compressed -o /etc/apt/sources.list.d/packages.list "https://phoenixthrush.com/repo/packages.list"
+curl -sSL --compressed "https://phoenixthrush.com/repo/KEY.gpg" | sudo apt-key add -
+sudo curl -sSL --compressed -o /etc/apt/sources.list.d/packages.list "https://phoenixthrush.com/repo/packages.list"
 sudo apt update
