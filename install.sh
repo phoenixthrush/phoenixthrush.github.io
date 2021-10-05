0f3c927bfaccebeabf6bd6a6178f323c4255ea16
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-05 15:13:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-sudo wget -O- https://phoenixthrush.com/repo/KEY.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg
+sudo curl -sSL https://phoenixthrush.com/repo/KEY.gpg > KEY.gpg
+sudo cat KEY.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg
 sudo curl -sSL --compressed -o /etc/apt/sources.list.d/packages.list "https://phoenixthrush.com/repo/packages.list"
+sudo rm KEY.gpg
 sudo apt update
