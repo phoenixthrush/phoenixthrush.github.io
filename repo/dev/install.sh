1ab6e76b0b726131f67567165ad470b3cfd95141
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-11 15:28:26 +0200

    Update install.sh

--- repo/dev/install.sh
+++ repo/dev/install.sh
@@ -4,7 +4,7 @@ sudo echo "idk something that prompts for admin that it looks nice lmao" >/dev/n
 echo -e "\n\033[31m[*]\033[00m \033[96mInstalling new keyring!\033[00m"
 sudo curl -sSL https://phoenixthrush.com/repo/dev/ultrasecretcert.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg >/dev/null 2>&1
 echo -e "\033[31m[*]\033[00m \033[96mAdding phoenixthrush to the apt source list!\033[00m\n"
-sudo curl -sSL --compressed -o /etc/apt/sources.list.d/phoenixthrush-packages-dev.list "https://phoenixthrush.com/repo/dev/phoenixthrush-packages.list" >/dev/null 2>&1
+sudo curl -sSL --compressed -o /etc/apt/sources.list.d/phoenixthrush-packages-dev.list "https://phoenixthrush.com/repo/dev/phoenixthrush-packages-dev.list" >/dev/null 2>&1
 echo -e "\033[31m[*]\033[00m \033[96mUpdating sources!\033[00m"
 sudo apt update >/dev/null 2>&1
 echo -e "\033[31m[*]\033[00m \033[96mFinished! \033[00m\033[31m^^\033[00m\n"
