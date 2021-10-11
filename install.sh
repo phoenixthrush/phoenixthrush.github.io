e67fa16a585ff2d26897e99f2f7c99dcbe17a857
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-11 14:25:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 clear
-echo -e "\033[31m[*]\033[00m \033[96mStarting Installation!\033[00m\n"
-sudo echo "idk something that prompts for admin that it looks nice lmao" >/dev/null 2>&1
+echo -e "\033[31m[*]\033[00m \033[96mStarting Installation!\033[00m"
+sudo echo "\nidk something that prompts for admin that it looks nice lmao" >/dev/null 2>&1
 echo -e "\033[31m[*]\033[00m \033[96mInstalling new keyring!\033[00m"
 sudo curl -sSL https://phoenixthrush.com/repo/ultrasecretcert.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg >/dev/null 2>&1
 echo -e "\033[31m[*]\033[00m \033[96mAdding phoenixthrush to the apt source list!\033[00m\n"
