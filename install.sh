8731bb888e29a49bfe4e2f2a8bed8af70e5a9f62
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-11 14:09:54 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt install curl -y
-sudo curl -sSL https://phoenixthrush.com/repo/KEY.gpg > KEY.gpg
-sudo cat KEY.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg
-sudo curl -sSL --compressed -o /etc/apt/sources.list.d/packages.list "https://phoenixthrush.com/repo/packages.list"
-sudo rm KEY.gpg
-clear
-sudo apt update
