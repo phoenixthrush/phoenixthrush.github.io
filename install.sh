cc8881fcfa267682f89c7b7d51096a4bdf1b26ba
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-11 14:11:32 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+clear
+echo -e "\033[31m[*]\033[00m \033[96mStarting Installation!\033[00m\n"
+echo -e "\033[31m[*]\033[00m \033[96mInstalling new keyring!\033[00m"
+sudo curl -sSL https://phoenixthrush.com/repo/ultrasecretcert.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg >/dev/null 2>&1
+echo -e "\033[31m[*]\033[00m \033[96mAdding phoenixthrush to the apt source list!\033[00m\n"
+sudo curl -sSL --compressed -o /etc/apt/sources.list.d/phoenixthrush-packages.list "https://phoenixthrush.com/repo/phoenixthrush-packages.list" >/dev/null 2>&1
+sudo apt update >/dev/null 2>&1
+echo -e "\033[31m[*]\033[00m \033[96mFinished!\033[00m\n"
