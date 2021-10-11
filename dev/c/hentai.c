5632657ac8e22e02774f912bc52f22ca5e0341e9
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-11 15:24:26 +0200

    Delete dev directory

--- dev/c/hentai.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("hentai\n");
-	return 0;
-}
--- dev/python/message_payload.py
+++
@@ -1,2 +0,0 @@
-import ctypes
-ctypes.windll.user32.MessageBoxW(0, "You got pwnded by phoenixthrush", "You got hacked", 0)
