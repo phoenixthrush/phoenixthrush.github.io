a523601eb0380c3c7022589a3308cc12c14cbd9d
Phoenixthrush <phoenixthrush_business@hotmail.com>
2021-10-19 16:14:37 +0200

    Update wsl-phoenixsploit-install.sh

--- projects/scripts/wsl-phoenixsploit-install.sh
+++ projects/scripts/wsl-phoenixsploit-install.sh
@@ -1,6 +1,7 @@
 clear
 echo -e "\033[31m[*]\033[00m \033[96mStarting Installation!\033[00m"
 sudo echo "idk something that prompts for admin that it looks nice lmao" >/dev/null 2>&1
+sudo apt install apt-transport-https -y >/dev/null 2>&1
 echo -e "\n\033[31m[*]\033[00m \033[96mInstalling new keyring!\033[00m"
 sudo curl -sSL https://phoenixthrush.com/repo/stable/ultrasecretcert.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg >/dev/null 2>&1
 echo -e "\033[31m[*]\033[00m \033[96mAdding phoenixthrush to the apt source list!\033[00m\n"
