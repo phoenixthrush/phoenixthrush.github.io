a5e7ed7b6ff1f367141fb710fc6b5e5178009971
Phoenixthrush <mr.cetto@phoenixthrush.com>
2021-11-23 15:32:35 +0100

    Delete projects/scripts directory

--- projects/scripts/decoder.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd %userprofile%\Desktop
-certutil -decode "%~1" "%~1".decoded
-del "%~1"
--- projects/scripts/encoder.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd %userprofile%\Desktop
-certutil -encode "%~1" tmp.b64 && findstr /v /c:- tmp.b64 > "%~1".b64
-del tmp.b64
